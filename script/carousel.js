235031db0dbd7d2c2c9f0dcde284303e4239d5de
sarthakjdev <jsarthak448@gmail.com>
2021-11-14 15:35:19 +0530

    [MOD] corrected corousel

--- script/carousel.js
+++ script/carousel.js
@@ -1,6 +1,5 @@
 const track = document.querySelector(".carousel-track");
 const slides = Array.from(track.children);
-// console.log("slides ", slides);
 
 const nextButton = document.querySelector(".carousel_button--right");
 const prevButton = document.querySelector(".carousel_button--left");
@@ -9,7 +8,7 @@ const dots = Array.from(dotsnav.children);
 // console.log("dots ", dots);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
-// console.log("slideWisth", slideWidth)
+console.log("slideWidth", slideWidth)
 
 //arrange the slides next to one another
 const setSlidePosition = (slide, index) => {
@@ -18,12 +17,27 @@ const setSlidePosition = (slide, index) => {
 slides.forEach(setSlidePosition);
 
 //when I click left, move slide to left
+prevButton.addEventListener('click', (e)=>{
+    const currentSlide = track.querySelector('.current-slide');
+ console.log("currentSlide ", currentSlide);
+    const nextSlide = currentSlide.previousElementSibling;
+ console.log("nextSlide ", nextSlide);
 
+    const amountToMove = nextSlide.style.left;
+    //move to the next slide
+    track.style.transform = 'translateX(-' + amountToMove + ')';
+    currentSlide.classList.remove('current-slide');
+    nextSlide.classList.add('current-slide');
+
+})
 
 //when I click right, move slide to right
-nextButton.addEventListener('click', e =>{
+nextButton.addEventListener('click', (e)=>{
     const currentSlide = track.querySelector('.current-slide');
+ console.log("currentSlide ", currentSlide);
     const nextSlide = currentSlide.nextElementSibling;
+ console.log("nextSlide ", nextSlide);
+
     const amountToMove = nextSlide.style.left;
     //move to the next slide
     track.style.transform = 'translateX(-' + amountToMove + ')';
